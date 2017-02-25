e87dfe2271b1a7a925632b665bfc554859ab57c1
Frederik Stensaeth <stensaethf@carleton.edu>
2017-02-25 11:50:35 -0600

    javadocs for CornerFinder.java

--- Crystal-AR/crystal-ar/src/main/java/com/crystal_ar/crystal_ar/CornerFinder.java
+++ Crystal-AR/crystal-ar/src/main/java/com/crystal_ar/crystal_ar/CornerFinder.java
@@ -6,124 +6,51 @@ import android.util.Log;
 import java.util.Set;
 import java.util.TreeMap;
 
-/**
- * Created by Sabastian on 2/14/17.
+/*
+ * Public class: CornerFinder.
+ * Class for finding corners in an image.
+ * Properties: paint_threshold, corner_threshold.
+ * Methods: findCorners.
+ * @params Bitmap - Image to find corners in.
+ * @params int (optional) - x-coordinate of the center of the table.
+ * @params int (optional) - y-coordinate of the center of the table.
+ * @params int (optional, if two first int parameters) - paint threshold.
+ * @params int (optional, unless third int parameter) - corner threshold.
+ * @return n/a.
  */
-
 public class CornerFinder {
-    /*
-     * a method to help debuging by printing out samples of the table in a grid
-     */
-    private void print_magic(int[][] magic, int step) {
-        String str = "";
-        System.out.println(magic.length + " : " + step);
-        for (int y = 0; y < magic[0].length; y += step) {
-            for (int x = 0; x < magic.length; x += step/2) {
-                str += (int) (magic[x][y]/1000);
-            }
-            str += "\n";
-        } 
-        System.out.println(str);
-    }
+    private int[] intense = new int[1];
+    private int[][] magic = new int[1][1];
+    private CrystalCustomQueue pixels_to_look_at = new CrystalCustomQueue();
 
     /*
-     * This function takes a list of border pixels and a theta.
-     * It then rotates the coordinate axes by theta, and finds the points with the smallest and
-     * largest points in terms of their (rotated) x- and y- coordiantes
-     *
-     * @param borders - 2d array in which 2 is a "point" in the set
-     * @param theta - angle of the "x-axis"
-     * @return array of the 4 extrema
+     * Properties.
      */
-    private IntPair[] find_extrema(CrystalCustomQueue borders, double theta) {
-        double cos = Math.cos(theta);
-        double sin = Math.sin(theta);
-        double adj;
-        double opp;
-
-        double min_adj = 1e300;
-        IntPair min_adj_pt = new IntPair(0, 0);
-        double max_adj = -1e300;
-        IntPair max_adj_pt = new IntPair(0, 0);
-        double min_opp = 1e300;
-        IntPair min_opp_pt = new IntPair(0, 0);
-        double max_opp = -1e300;
-        IntPair max_opp_pt = new IntPair(0, 0);
-
-        for (int i = 0; i < borders.size(); ++i) {
-            int pixel = borders.get(i);
-            int x = pixel / 100000;
-            int y = pixel % 100000;
-            adj = cos * x + sin * y;
-            opp = -1 * sin * x + cos * y;
-
-            if (adj > max_adj) {
-                max_adj = adj;
-                max_adj_pt.x = x;
-                max_adj_pt.y = y;
-            }
-            if (adj < min_adj) {
-                min_adj = adj;
-                min_adj_pt.x = x;
-                min_adj_pt.y = y;
-            }
-            if (opp > max_opp) {
-                max_opp = opp;
-                max_opp_pt.x = x;
-                max_opp_pt.y = y;
-            }
-            if (opp < min_opp) {
-                min_opp = opp;
-                min_opp_pt.x = x;
-                min_opp_pt.y = y;
-            }
-        }
-
-        IntPair[] rtn = new IntPair[4];
-        rtn[0] = min_adj_pt; // smallest "x"
-        rtn[1] = min_opp_pt; // smallest "y"
-        rtn[2] = max_adj_pt; // largest "x"
-        rtn[3] = max_opp_pt; // largest "y"
-        return rtn;
-    }
+    public int paint_threshold = 5; // Paint threshold.
+    public int corner_threshold = 20; // Corner threshold.
 
     /*
-     * Finds the key in a dictionary (map) with the largest value
-     *
-     * @param dict - the dictionary to search
-     * @param IntPair - the key whose value is the maximum
+     * Finds corners in an image assuming that the center of the table is in the middle of the
+     * image.
+     * @params Bitmap - image to find corners in.
+     * @return IntPair[] - list of corner coordinates.
      */
-    IntPair find_key_with_largest_value(TreeMap<IntPair, Integer> dict) {
-        Set<IntPair> keys = dict.keySet();
-        int max_val = -100;
-        IntPair max_key = null;
-        for (IntPair key : keys) {
-            if (!dict.containsKey(key)) throw new RuntimeException("find_key_with_largest_value given no keys");
-            int val = dict.get(key);
-            if (val > max_val) {
-                max_val = val;
-                max_key = key;
-            }
-        }
-        return max_key;
-    }
-
-    private int compute_pixel_diff(int x, int y) {
-        return Math.abs((0xFF000000 & x) - (0xFF000000 & y)) / 16777216 + Math.abs((0x00FF0000 & x) - (0x00FF0000 & y)) / 65536 + Math.abs((0x0000FF00 & x) - (0x0000FF00 & y)) / 256;
-    }
-
     public IntPair[] findCorners(Bitmap img) throws NullPointerException, IllegalArgumentException, RuntimeException {
         if (img == null)
             throw new NullPointerException("findCorners(img) given null for image");
         return findCorners(img, img.getWidth()/2, img.getHeight()/2);
     }
 
-    private int[] intense = new int[1];
-    private int[][] magic = new int[1][1];
-    private CrystalCustomQueue pixels_to_look_at = new CrystalCustomQueue();
-    public int paint_threshold = 5;
-    public int corner_threshold = 20;
-
+    /*
+     * Finds corners in an image that passes certain thresholds and where the center of the table is
+     * at the given position.
+     * @params Bitmap - image to find corners in.
+     * @params int - x-coordinate of the center of the table.
+     * @params int - y-coordinate of the center of the table.
+     * @params int - paint threshold.
+     * @params int - corner threshold.
+     * @return IntPair[] - list of corner coordinates.
+     */
     public IntPair[] findCorners(Bitmap img, int center_x, int center_y, int pt, int ct) throws NullPointerException, IllegalArgumentException, RuntimeException {
         paint_threshold = pt;
         corner_threshold = ct;
@@ -131,10 +58,11 @@ public class CornerFinder {
     }
 
     /*
-     * Find the corners of a table near the center of the image
-     *
-     * @param img - the bitmap to find the table of
-     * @return a set of corner-coordinates
+     * Finds corners in an image where the center of the table is at the given position.
+     * @params Bitmap - image to find corners in.
+     * @params int - x-coordinate of the center of the table.
+     * @params int - y-coordinate of the center of the table.
+     * @return IntPair[] - list of corner coordinates.
      */
     public IntPair[] findCorners(Bitmap img, int center_x, int center_y) throws NullPointerException, IllegalArgumentException, RuntimeException {
         if (img == null)
@@ -334,6 +262,119 @@ public class CornerFinder {
         }
 
         return corners;
+    }
+
+    /*
+     * a method to help debuging by printing out samples of the table in a grid
+     */
+
+    /*
+     * Prints magic.
+     * at the given position.
+     * @params int[][] - magic.
+     * @params int - step.
+     * @return void.
+     */
+    private void print_magic(int[][] magic, int step) {
+        String str = "";
+        System.out.println(magic.length + " : " + step);
+        for (int y = 0; y < magic[0].length; y += step) {
+            for (int x = 0; x < magic.length; x += step/2) {
+                str += (int) (magic[x][y]/1000);
+            }
+            str += "\n";
+        } 
+        System.out.println(str);
+    }
+
+    /*
+     * This function takes a list of border pixels and a theta.
+     * It then rotates the coordinate axes by theta, and finds the points with the smallest and
+     * largest points in terms of their (rotated) x- and y- coordinates.
+     * @params CrystalCustomQueue - borders: 2d array in which 2 is a "point" in the set.
+     * @params double - theta: angle of the "x-axis".
+     * @return IntPair[] - 4 extrema.
+     */
+    private IntPair[] find_extrema(CrystalCustomQueue borders, double theta) {
+        double cos = Math.cos(theta);
+        double sin = Math.sin(theta);
+        double adj;
+        double opp;
+
+        double min_adj = 1e300;
+        IntPair min_adj_pt = new IntPair(0, 0);
+        double max_adj = -1e300;
+        IntPair max_adj_pt = new IntPair(0, 0);
+        double min_opp = 1e300;
+        IntPair min_opp_pt = new IntPair(0, 0);
+        double max_opp = -1e300;
+        IntPair max_opp_pt = new IntPair(0, 0);
+
+        for (int i = 0; i < borders.size(); ++i) {
+            int pixel = borders.get(i);
+            int x = pixel / 100000;
+            int y = pixel % 100000;
+            adj = cos * x + sin * y;
+            opp = -1 * sin * x + cos * y;
+
+            if (adj > max_adj) {
+                max_adj = adj;
+                max_adj_pt.x = x;
+                max_adj_pt.y = y;
+            }
+            if (adj < min_adj) {
+                min_adj = adj;
+                min_adj_pt.x = x;
+                min_adj_pt.y = y;
+            }
+            if (opp > max_opp) {
+                max_opp = opp;
+                max_opp_pt.x = x;
+                max_opp_pt.y = y;
+            }
+            if (opp < min_opp) {
+                min_opp = opp;
+                min_opp_pt.x = x;
+                min_opp_pt.y = y;
+            }
+        }
+
+        IntPair[] rtn = new IntPair[4];
+        rtn[0] = min_adj_pt; // smallest "x"
+        rtn[1] = min_opp_pt; // smallest "y"
+        rtn[2] = max_adj_pt; // largest "x"
+        rtn[3] = max_opp_pt; // largest "y"
+        return rtn;
+    }
+
+    /*
+     * Finds the key in a dictionary (map) with the largest value.
+     * @params TreeMap - the dictionary to search.
+     * @return IntPair - the key whose value is the maximum.
+     */
+    IntPair find_key_with_largest_value(TreeMap<IntPair, Integer> dict) {
+        Set<IntPair> keys = dict.keySet();
+        int max_val = -100;
+        IntPair max_key = null;
+        for (IntPair key : keys) {
+            if (!dict.containsKey(key)) throw new RuntimeException("find_key_with_largest_value given no keys");
+            int val = dict.get(key);
+            if (val > max_val) {
+                max_val = val;
+                max_key = key;
+            }
+        }
+        return max_key;
+    }
+
+    /*
+     * Computes the pixel difference.
+     * @params int - first pixel.
+     * @params int - second pixel.
+     * @return int - pixel difference.
+     */
+    private int compute_pixel_diff(int x, int y) {
+        return Math.abs((0xFF000000 & x) - (0xFF000000 & y)) / 16777216 + Math.abs((0x00FF0000 & x) - (0x00FF0000 & y)) / 65536 + Math.abs((0x0000FF00 & x) - (0x0000FF00 & y)) / 256;
     }
 }
 
